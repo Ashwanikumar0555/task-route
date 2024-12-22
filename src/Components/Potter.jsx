0c557fc81b4c444459c26fbfcefec75866b92ac9
Ashwani kumar <ashwanikumar05556@gmail.com>
2024-12-23 00:02:04 +0530

    project task route

--- src/Components/Potter.jsx
+++ src/Components/Potter.jsx
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 
 const Potter = () => {
@@ -22,7 +28,14 @@ const Potter = () => {
       ) : (
         <ul>
           {characters.slice(0, 10).map((char) => (
-            <li key={char.name}>{char.name}</li>
+            <li key={char.name}>
+              <img
+                src={char.image || 'https://via.placeholder.com/150'}
+                alt={char.name}
+                style={{ width: '150px', height: '200px' }}
+              />
+              <p>{char.name}</p>
+            </li>
           ))}
         </ul>
       )}
@@ -31,3 +44,8 @@ const Potter = () => {
 };
 
 export default Potter;
+
+
+
+
+
